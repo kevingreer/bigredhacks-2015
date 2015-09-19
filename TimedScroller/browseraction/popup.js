1361b2b3c8478d8ef47ce495ee3295f0d5fa061f
Yungton Yang <yungtonyang@dhcp-ccc-1409.eduroam.cornell.edu>
2015-09-19 18:03:57 -0400

    changes

--- TimedScroller/browseraction/popup.js
+++ TimedScroller/browseraction/popup.js
@@ -1,11 +1,4 @@
-window.onload = function() {
-	document.getElementById("button").onclick = function() {
-		console.log(document.getElementById("interval").value);
-		//chrome.tabs.executeScript(null, {file: "browseraction/scroll.js"});
-		/*chrome.tabs.executeScript(null, {
-   			 code: 'var time = parseInt(document.getElementById("interval").value);'
-		}, function() {
-    		chrome.tabs.executeScript(null, {file: 'browseraction/scroll.js'});
-		});*/
-	}
+document.getElementById("button").onclick = function() {
+	//chrome.tabs.executeScript(null, {file: "browseraction/scroll.js"});
+	chrome.tabs.executeScript(null, {code: 'var time = 1000;'}, function() {chrome.tabs.executeScript(null, {file: 'browseraction/scroll.js'});});
 }
