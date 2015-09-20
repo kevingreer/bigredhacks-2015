8a566520bb22591ee41265b21f85c4ed5106cd98
kevingreer <kevingreer@me.com>
2015-09-19 21:14:32 -0400

    thing

--- TimedScroller/browseraction/popup.js
+++ TimedScroller/browseraction/popup.js
@@ -3,8 +3,12 @@ var textField = document.getElementById("interval");
 document.getElementById("button").onclick = function() {
   if(textField.value < 4)
     textField.value = 4;
+  
+  var mstime = parseInt(textField.value);
+  console.log("swag");
+
   chrome.tabs.executeScript(null, {
-      code: 'var time = ' + (parseInt(textField.value)*1000) + ';'
+      code: 'var time = ' + (mstime*1000) + ';'
   }, function() {
     chrome.tabs.executeScript(null, {file: 'browseraction/scroll.js'});
   });
