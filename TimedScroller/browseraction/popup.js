d7957fd31610815fca4e9972f250c036818fec7f
Yungton Yang <yungtonyang@dhcp-ccc-1409.eduroam.cornell.edu>
2015-09-19 20:51:10 -0400

    Everything merged together

--- TimedScroller/browseraction/popup.js
+++ TimedScroller/browseraction/popup.js
@@ -1,8 +1,10 @@
 var textField = document.getElementById("interval");
 
 document.getElementById("button").onclick = function() {
+  if(textField.value < 4)
+    textField.value = 4;
   chrome.tabs.executeScript(null, {
-       code: 'var time = ' + (parseInt(textField.value)*1000) + ';'
+      code: 'var time = ' + (parseInt(textField.value)*1000) + ';'
   }, function() {
     chrome.tabs.executeScript(null, {file: 'browseraction/scroll.js'});
   });
