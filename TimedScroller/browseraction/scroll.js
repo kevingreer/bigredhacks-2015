e354e82165a28a7d38cb17222a3893b485255a66
kevingreer <kevingreer@me.com>
2015-09-19 20:24:33 -0400

    smoothscroll

--- TimedScroller/browseraction/scroll.js
+++ TimedScroller/browseraction/scroll.js
@@ -1,3 +1,27 @@
-// alert(time);
-var HEIGHT = window.innerHeight;
-setInterval( function() { window.scrollBy(0, HEIGHT - 100); }, time);
+var getCurrentScrollHeight = function() {
+  return document.body.scrollTop
+}
+
+var smoothScroll = function(distance, duration) { //duration in milliseconds
+  var STEP_SIZE = 1;
+  var endY = getCurrentScrollHeight() + distance;
+  var steps = distance/STEP_SIZE;
+  var incTime = duration/steps;
+
+  // If the current position is less than the end position, scroll by the step
+  // size every time interval. Otherwise, scroll to the end position and stop 
+  // the timer.
+  var timer = setInterval( function () {
+    var currentY = getCurrentScrollHeight();
+    if (currentY < endY) {
+      window.scrollBy(0, STEP_SIZE);
+    } else {
+      window.scrollTo(0, endY);
+      clearInterval(timer);
+    }
+  }, incTime);
+}
+
+setInterval( function() { 
+  smoothScroll(window.innerHeight - 100, 100);
+}, time);
